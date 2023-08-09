dcb491eee8e3a8300c02423cecf3c61a6211947e
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-08-10 02:54:32 +0300

    changes made

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
-function Home({ isSignedIn }) {
-  // if the user isn't signed in, redirect them to the login page
-  if (!isSignedIn) return <Redirect to="/login" />;
+function Home({ isLoggedIn }) {
+  if (!isLoggedIn) return <Redirect to="/login" />;
 
-  // otherwise, return the home page
   return (
     <div>
       <h1>Home!</h1>
@@ -13,14 +11,4 @@ function Home({ isSignedIn }) {
   );
 }
 
-// Wrap the Home component inside a Route component
-function App() {
-  const isSignedIn = true; 
-  return (
-    <Route exact path="/">
-      <Home isSignedIn={isSignedIn} />
-    </Route>
-  );
-}
-
-export default App;
+export default Home;
