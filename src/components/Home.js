51d1a9071fbabbe8988450818df4365649dc51a3
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-08-10 02:56:38 +0300

    Corrected

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
-function Home({ isLoggedIn }) {
-  if (!isLoggedIn) return <Redirect to="/login" />;
+function Home({ isSignedIn }) {
+  // if the user isn't signed in, redirect them to the login page
+  if (!isSignedIn) return <Redirect to="/login" />;
 
+  // otherwise, return the home page
   return (
     <div>
       <h1>Home!</h1>
@@ -11,4 +13,14 @@ function Home({ isLoggedIn }) {
   );
 }
 
-export default Home;
+// Wrap the Home component inside a Route component
+function App() {
+  const isSignedIn = true; 
+  return (
+    <Route exact path="/">
+      <Home isSignedIn={isSignedIn} />
+    </Route>
+  );
+}
+
+export default App;
